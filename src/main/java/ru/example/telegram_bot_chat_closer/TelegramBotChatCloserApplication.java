82a80c94186485de1e00a6a6b10904cc5cab6cb8
ShuMAhd <shuman@zykaika.ru>
2024-07-25 22:13:48 +0800

    first commit

---
+++ src/main/java/ru/example/telegram_bot_chat_closer/TelegramBotChatCloserApplication.java
@@ -0,0 +1,13 @@
+package ru.example.telegram_bot_chat_closer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotChatCloserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotChatCloserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/example/telegram_bot_chat_closer/TelegramBotChatCloserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.telegram_bot_chat_closer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotChatCloserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
